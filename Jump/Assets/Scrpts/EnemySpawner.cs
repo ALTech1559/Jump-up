1a714a8a5709a327c73bbce381dca13b1f97cd02
ALTech1559 <a6271547@mail.ru>
2022-10-17 13:15:15 +0300

    replace files

---
+++ Jump/Assets/Scrpts/EnemySpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject Enemy;
+    [SerializeField] private Transform LeftPoint;
+    [SerializeField] private Transform RightPoint;
+    [Header("Pause in seconds")]
+    [SerializeField] private float pause;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        while (true)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(pause);
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        GameObject enemy = Instantiate(Enemy);
+        enemy.transform.parent = this.transform;
+        enemy.transform.position = new Vector2(Random.Range(LeftPoint.position.x, RightPoint.position.x), LeftPoint.position.y); 
+    }
+}
---
+++ Jump/Assets/Scrpts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{  
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float maximumJumpHeight;
+    [SerializeField] private int startJumpsCount;
+    [SerializeField] private int currentJumpsCount = 0;
+    private Rigidbody2D rigidbody;
+    private float yForce = 0;
+    private float startJumpPositionY = 0;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        currentJumpsCount = startJumpsCount;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && currentJumpsCount > 0)
+        {
+            currentJumpsCount--;
+            yForce = jumpForce;
+        }
+
+        float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        Vector2 moveDirection = new Vector2(x, yForce);
+        transform.Translate(moveDirection);
+
+        if ()
+    }
+
+    internal void RefreshJumpsCount()
+    {
+        currentJumpsCount = startJumpsCount;
+    }
+}
