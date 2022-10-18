fafdabd70414e1177c966e781ad71b52c8d2908e
ALTech1559 <a6271547@mail.ru>
2022-10-18 20:41:10 +0300

    add particals

--- Jump/Assets/Scrpts/JumpBonus.cs
+++ Jump/Assets/Scrpts/JumpBonus.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class JumpBonus : MonoBehaviour
 {
+    [SerializeField] private float destroyPause;
     [SerializeField] private int jumpsCount;
+    [SerializeField] private ParticleSystem effect;
     internal delegate void DestroyMyselfHandler();
     internal event DestroyMyselfHandler destroyMyself;
 
     internal void DeleteMyself()
     {
         destroyMyself.Invoke();
-        Destroy(this.gameObject);
+        effect.Play();
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<CircleCollider2D>().enabled = false;
+        Destroy(this.gameObject, destroyPause);
     }
 
     internal int GetJumpsCount
